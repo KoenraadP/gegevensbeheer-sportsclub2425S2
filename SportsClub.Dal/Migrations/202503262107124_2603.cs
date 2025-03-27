40f8633f7ac9ae7fb6089e0ac297e60cd5227b91
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2025-03-27 15:48:58 +0100

    27031548

---
+++ SportsClub.Dal/Migrations/202503262107124_2603.cs
@@ -0,0 +1,56 @@
+﻿namespace SportsClub.Dal.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _2603 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Activities",
+                c => new
+                    {
+                        ActivityId = c.Int(nullable: false, identity: true),
+                        Activityname = c.String(),
+                        MaxParticipants = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ActivityId);
+            
+            CreateTable(
+                "dbo.Members",
+                c => new
+                    {
+                        MemberId = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 30),
+                        LastName = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.MemberId);
+            
+            CreateTable(
+                "dbo.MemberActivities",
+                c => new
+                    {
+                        Member_MemberId = c.Int(nullable: false),
+                        Activity_ActivityId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Member_MemberId, t.Activity_ActivityId })
+                .ForeignKey("dbo.Members", t => t.Member_MemberId, cascadeDelete: true)
+                .ForeignKey("dbo.Activities", t => t.Activity_ActivityId, cascadeDelete: true)
+                .Index(t => t.Member_MemberId)
+                .Index(t => t.Activity_ActivityId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.MemberActivities", "Activity_ActivityId", "dbo.Activities");
+            DropForeignKey("dbo.MemberActivities", "Member_MemberId", "dbo.Members");
+            DropIndex("dbo.MemberActivities", new[] { "Activity_ActivityId" });
+            DropIndex("dbo.MemberActivities", new[] { "Member_MemberId" });
+            DropTable("dbo.MemberActivities");
+            DropTable("dbo.Members");
+            DropTable("dbo.Activities");
+        }
+    }
+}
